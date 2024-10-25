eb2fb785cb3c2477b56d3220a1543df5ef7a3901
luis sensat <luis.sensat@gmail.com>
2024-10-25 21:03:20 +0200

    starting some styling

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -11,15 +11,13 @@ export default function Products(){
       try{
         const response = await fetch('http://localhost:3000/api/v1/products');
 
-        console.log(response)
-
         if(!response.ok){
           throw new Error("Could not retreive products")
         }
 
         const resData = await response.json();
         setProducts(resData);
-        console.log("hello")
+
         setLoading(false);
       } catch(error){
         setError({message: error.message || "Could not retreive products"})
@@ -40,16 +38,16 @@ export default function Products(){
   return(
     <div>
       <h2>Products</h2>
-      <ul>
+      <div className="product-card">
         {products.map(product => (
-          <li key={product.id}>
+          <div key={product.id}>
             <h3>{product.name}</h3>
             <h4>{product.name}</h4>
             <p>{product.description}</p>
             <p>{product.price}</p>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   )
 }
